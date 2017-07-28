23ff9a7c05d65f897ac15efa68c252714693f3e4
rickuster <rickuster@gmail.com>
2017-07-28 01:02:59 -0700

    finished address bloc menu assignment

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -11,10 +11,11 @@ require_relative '../models/address_book'
 
      puts "Main Menu - #{address_book.entries.count} entries"
      puts "1 - View all entries"
-     puts "2 - Create an entry"
-     puts "3 - Search for an entry"
-     puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "2 - View Entry Number n"   
+     puts "3 - Create an entry"
+     puts "4 - Search for an entry"
+     puts "5 - Import entries from a CSV"
+     puts "6 - Exit"
      print "Enter your selection: "
  
      selection = gets.to_i
@@ -26,17 +27,21 @@ require_relative '../models/address_book'
          main_menu
        when 2
          system "clear"
-         create_entry
+         view_entry_number_n
          main_menu
        when 3
          system "clear"
-         search_entries
+         create_entry
          main_menu
        when 4
          system "clear"
-         read_csv
+         search_entries
          main_menu
        when 5
+         system "clear"
+         read_csv
+         main_menu
+       when 6
          puts "Good-bye!"
          # #8
          exit(0)
@@ -84,22 +89,20 @@ require_relative '../models/address_book'
    end
 
    def entry_submenu(entry)
-     # #16
+
      puts "n - next entry"
      puts "d - delete entry"
      puts "e - edit this entry"
      puts "m - return to main menu"
  
-     # #17
+
      selection = gets.chomp
  
      case selection
-     # #18
+
        when "n"
-     # #19
        when "d"
        when "e"
-     # #20
        when "m"
          system "clear"
          main_menu
@@ -108,6 +111,22 @@ require_relative '../models/address_book'
          puts "#{selection} is not a valid input"
          entry_submenu(entry)
      end
+   end
+
+   def view_entry_number_n
+      puts "Enter the entry number to view"
+      input = gets.chomp.to_i
+      if input > address_book.entries.length
+          puts "Invalid entry"
+          view_entry_number_n
+      else
+          input = input - 1
+          system "clear"
+          puts address_book.entries[input]
+          puts "Press enter to return to the main menu"
+          gets.chomp
+          system "clear"
+      end
    end
 
  end
